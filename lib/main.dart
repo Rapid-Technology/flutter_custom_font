a25156c2a7a55a92509069e791c38ba96f970e04
Yasin <yasinm289@gmail.com>
2021-11-27 21:21:50 +0430

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -23,36 +23,7 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
         brightness: Brightness.dark,
       ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Remachine Script",
-              style: TextStyle(fontSize: 24),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 10, bottom: 5),
-              child: Text(
-                "Flutter is a cross-platform UI toolkit that is designed to allow code reuse across operating systems such as iOS and Android, while also allowing applications to interface directly with underlying platform services. The goal is to enable developers to deliver high-performance apps that feel natural on different platforms, embracing differences where they exist while sharing as much code as possible.",
-                style: TextStyle(
-                  height: 1.4,
-                  fontSize: 16,
-                ),
-              ),
-            ),
-            Text(
-              "During development, Flutter apps run in a VM that offers stateful hot reload of changes without needing a full recompile. For release, Flutter apps are compiled directly to machine code, whether Intel x64 or ARM instructions, or to JavaScript if targeting the web. The framework is open source, with a permissive BSD license, and has a thriving ecosystem of third-party packages that supplement the core library functionality.",
-              style: TextStyle(
-                height: 1.4,
-                fontSize: 16,
-              ),
-            ),
-          ],
-        ),
-      ),
+      body: Container(),
     );
   }
 }
